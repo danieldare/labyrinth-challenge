ff8cb30c676d89036993bb55265cdb68498e9008
danieldare-plateaumed <doluwadare@plateaumed.com>
2024-08-30 19:00:17 +0100

    feat(challange): add logic to solve the shortest path

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,1 +1,12 @@
-describe("Labyrinth shortest path", () => {});
+import { shortestPath } from "..";
+
+describe("Labyrinth shortest path", () => {
+  it("should find the shortest path in a simple labyrinth", () => {
+    const labyrinth = [
+      ["S", "0", "E"],
+      ["1", "0", "1"],
+      ["1", "0", "0"],
+    ];
+    expect(shortestPath(labyrinth)).toBe(2);
+  });
+});
